1a60b191851be955e6b2b2e01bcc9e59ce1c2d7e
mohtakween <154629474+mohtakween@users.noreply.github.com>
2023-12-22 23:57:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-// Variables
+// Generate a random number between 1 and 100
 const targetNumber = Math.floor(Math.random() * 100) + 1;
 let remainingAttempts = 5;
-let currentLevel = 1;
 
 function checkGuess() {
   const guess = parseInt(document.getElementById("guessInput").value);
 
-  // Update remaining attempts and level progress
+  // Update remaining attempts
   remainingAttempts--;
   document.getElementById("remainingAttempts").textContent = `Attempts remaining: ${remainingAttempts}`;
 
-  // Check guess and provide feedback based on level range
+  // Check guess and provide feedback
   if (guess === targetNumber) {
-    document.getElementById("feedbackText").textContent = `Correct! You conquered level ${currentLevel} in ${5 - remainingAttempts} attempts.`;
-    currentLevel++; // Increase level on win
-    document.getElementById("levelText").textContent = `Level: ${currentLevel}`; // Update displayed level
-    startNewLevel(); // Reset for next level
+    document.getElementById("feedbackText").textContent = "Correct! You guessed the number in " + (5 - remainingAttempts) + " attempts.";
+    disableGuessInput(); // prevent further guesses
   } else if (guess > targetNumber) {
     document.getElementById("feedbackText").textContent = "Too high!";
   } else {
@@ -24,21 +21,11 @@ function checkGuess() {
 
   // Check if game is over
   if (remainingAttempts === 0 && guess !== targetNumber) {
-    document.getElementById("feedbackText").textContent = `Game over! The correct number for level ${currentLevel} was ${targetNumber}`;
+    document.getElementById("feedbackText").textContent = "Game over! The correct number was " + targetNumber;
     disableGuessInput(); // prevent further guesses
   }
-
-  // Additional logic specific to level progression, bonuses, etc.
 }
 
-function startNewLevel() {
-  // Update target number based on new level range
-  // Award bonus points/attempts?
-  // Reset remaining attempts
-  remainingAttempts = 5;
-  document.getElementById("remainingAttempts").textContent = `Attempts remaining: ${remainingAttempts}`;
-  // Generate new target number within updated range
-  targetNumber = ... // Implement updated number generation logic
-  // Clear guess input
-  document.getElementById("guessInput").value = "";
+function disableGuessInput() {
+  document.getElementById("guessInput").disabled = true;
 }
